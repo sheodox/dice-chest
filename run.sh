4d9b1e7b11a8b8d475a386f74013c709b59c0149
sheodox <imagamer27@msn.com>
2023-05-14 20:03:42 -0500

    Added a build/run script

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+deploy_target=$1
+
+if [[ $deploy_target == "deploy" ]]; then
+	npm run build
+	PORT=5005 node build
+elif [[ $deploy_target  == "run" ]]; then
+	PORT=5005 node build
+else
+	echo "Invalid deployment target specified! Must run as either:"
+	echo "  ./run.sh deploy"
+	echo "  ./run.sh run"
+fi
+
+
